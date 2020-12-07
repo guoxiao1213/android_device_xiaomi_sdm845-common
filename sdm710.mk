PixelExperience-Devices/device_xiaomi_sdm710-common

sdm710-common: Use component-override to configure crosshatch Bluetooth
Runtime Resource Overlays (RROs) can no longer change the value of
resources in manifest when read during PackagerParser.

This change uses component-override to enable the service by default
on crosshatch devices.

Remove usages of profile_supported_sap from overlays as they no
longer configure the enable state of .sap.SapService.

Bug: 135048762
Test: adb shell pm query-services \
      com.android.bluetooth/.sap.SapService

Change-Id: I89ba2aa61492f65b269849597282594507d2fdc3
Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

sdm710.mk
@@ -111,6 +111,9 @@ PRODUCT_PACKAGES += \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \

guoxiao1213/android_device_xiaomi_sdm845-common

sdm710.mk
@@ -19,12 +19,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay \
-    $(COMMON_PATH)/overlay-lineage \
-    $(COMMON_PATH)/overlay-system
+    $(COMMON_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(COMMON_PATH)/overlay-system
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -123,6 +120,9 @@ PRODUCT_PACKAGES += \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 # Camera
 PRODUCT_PACKAGES += \