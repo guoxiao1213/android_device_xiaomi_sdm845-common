d0c7cc427bc7d125a77012e1eab3579f713de351
guoxiao1213 <598464949@qq.com>
2021-01-03 13:36:39 +0800

    sdm710-common: Add component-overrides_qti.xml to support pure AOSP/QTI BT stack

--- sdm710.mk
+++ sdm710.mk
@@ -124,7 +124,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+    $(COMMON_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
 # Camera
 PRODUCT_PACKAGES += \
